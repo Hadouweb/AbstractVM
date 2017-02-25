f642ceb46a05f97ff75a7594c6a28bb6d949d926
Hadouweb <kengo14@hotmail.fr>
2017-02-25 01:06:51 +0100

    ok

--- srcs/VirtualMachine.class.cpp
+++ srcs/VirtualMachine.class.cpp
@@ -88,7 +88,6 @@ void VirtualMachine::execInstrDump(std::list<ParsedNode *>::iterator &it) {
 
 void VirtualMachine::execInstrAssert(std::list<ParsedNode *>::iterator &it) {
 	IOperand const * opTop = *this->_OpStack.begin();
-	std::cout << "LOL" << std::endl;
 	std::string value = getValueOnly((*it)->getValue());
 	if (opTop && (opTop->toString().compare(value) != 0 || opTop->getType() != this->getEnumOperand((*it)->getTkValue())))
 		throw VirtualMachine::AssertException();
